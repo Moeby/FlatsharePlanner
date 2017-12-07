2b0c0f29d75d9647ac755a139522337acdfe62c9
teshi24 <nadja.stadi@gmail.com>
2017-12-07 16:11:02 +0100

    Sprint 2 #44: implement methods GroupDAO
    not testet yet, waiting on Connection and DataClasses

--- Flattie/app/src/main/java/com/tbz/mntn/flattie/db/GroupDAO.java
+++ Flattie/app/src/main/java/com/tbz/mntn/flattie/db/GroupDAO.java
@@ -1,7 +1,12 @@
 package com.tbz.mntn.flattie.db;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupDAO {
     private static GroupDAO instance = new GroupDAO();
+    private static List<Group> groups = new Group();
 
     // table constants
     private static final String TABLE = "group";
@@ -15,23 +20,166 @@ public class GroupDAO {
         return instance;
     }
 
-    public void insert(){
-        // TODO: #44 implement method
-        // removal_date = null
+    // TESTME: #44
+    public int insert(Group group){
+        // TODO: #44 get connection
+        int rows                = -1;
+        Connection con          = null;
+        PreparedStatement stmt  = null;
+        ResultSet result        = null;
+        try{
+            stmt = con.prepareStatement("INSERT INTO " + TABLE + " (" + NAME + "," + REMOVAL_DATE + ")"
+                            + " VALUES( ?, ?);"
+                    , Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, group.getName());
+            stmt.setString(2, group.getRemovalDate());
+
+            rows = stmt.executeUpdate();
+            try(ResultSet generatedKeys = stmt.getGeneratedKeys()){
+                if(generatedKeys.next()){
+                    group.setId(generatedKeys.getInt(1));
+                }
+            }
+
+            if(rows > 0){
+                groups.add(group);
+            }
+
+        } catch (SQLException e){
+            // TODO: #44 implement errorhandling
+        } finally {
+            try  {
+                // free resources
+                if (result != null)
+                    result.close();
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException e) {
+               // TODO: #44 implement errorhandling
+                System.out.println("Statement or result close failed");
+            }
+        }
+        return rows;
     }
 
-
-    public void selectById(){
-        // TODO: #44 implement method
+    // TESTME: #44
+    public Group selectById(int id){
+        // TODO: #44 get connection
+        Group group             = null;
+        Connection con          = null;
+        PreparedStatement stmt  = null;
+        ResultSet result        = null;
+        try {
+            stmt = con.prepareStatement("SELECT " + NAME + " FROM " + TABLE
+                                            + " WHERE " + ID + " = ?;");
+            stmt.setInt(1, id);
+            result = stmt.executeQuery();
+            if(result.next()){
+                for(Group savedGroup: groups){
+                    if(id == savedGroup.getId()){
+                        group = savedGroup;
+                        break;
+                    }
+                }
+                if(group != null) {
+                    group.setId(id);
+                    group.setName(result.getString(NAME));
+                    groups.add(group);
+                }
+            }else{
+                group = null;
+            }
+        }catch (SQLException e){
+            // TODO: #44 implement errorhandling
+        }finally {
+            try  {
+                // free resources
+                if (result != null)
+                    result.close();
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException e) {
+                // TODO: #44 implement errorhandling
+                System.out.println("Statement or result close failed");
+            }
+        }
+        return group;
     }
 
-    public void remove(){
-        // TODO: #44 implement method
-        // update removal date --> insert current date
+    // TESTME: #44
+    public int remove(Group group){
+        // TODO: #44 get connection
+        Date removalDate        = new Date(new java.util.Date().getTime());
+        int rows                = -1;
+        Connection con          = null;
+        PreparedStatement stmt  = null;
+        ResultSet result        = null;
+        try{
+            stmt = con.prepareStatement("UPDATE " + TABLE +
+                            " SET " + REMOVAL_DATE + " = ?;");
+            stmt.setDate(1, removalDate);
+
+            rows = stmt.executeUpdate();
+
+            if(rows > 0){
+                // TODO: #44 check if needed
+                groups.remove(group);
+                group.setRemovalDate(removalDate);
+                groups.add(group);
+            }
+
+        } catch (SQLException e){
+            // TODO: #44 implement errorhandling
+        } finally {
+            try  {
+                // free resources
+                if (result != null)
+                    result.close();
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException e) {
+                // TODO: #44 implement errorhandling
+                System.out.println("Statement or result close failed");
+            }
+        }
+        return rows;
     }
 
-    public void reactivate(){
-        // TODO: #44 implement method
+    // TESTME: #44
+    public int reactivate(Group group){
         // at the moment no required feature!
+        // TODO: #44 get connection
+        int rows                = -1;
+        Connection con          = null;
+        PreparedStatement stmt  = null;
+        ResultSet result        = null;
+        try{
+            stmt = con.prepareStatement("UPDATE " + TABLE +
+                    " SET " + REMOVAL_DATE + " = ?;");
+            stmt.setDate(1, null);
+
+            rows = stmt.executeUpdate();
+
+            if(rows > 0){
+                // TODO: #44 check if needed
+                groups.remove(group);
+                group.setRemovalDate(null);
+                groups.add(group);
+            }
+        } catch (SQLException e){
+            // TODO: #44 implement errorhandling
+        } finally {
+            try  {
+                // free resources
+                if (result != null)
+                    result.close();
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException e) {
+                // TODO: #44 implement errorhandling
+                System.out.println("Statement or result close failed");
+            }
+        }
+        return rows;
     }
 }
