8f7a26a7b055dad80c0c3ad3d7452fdeb4059af4
teshi24 <nadja.stadi@gmail.com>
2017-12-14 07:40:51 +0100

    #44: errorhandling improved

--- Flattie/app/src/main/java/com/tbz/mntn/flattie/db/DAO.java
+++ Flattie/app/src/main/java/com/tbz/mntn/flattie/db/DAO.java
@@ -2,18 +2,35 @@ package com.tbz.mntn.flattie.db;
 
 
 public abstract class DAO {
+
+    /**
+     * change sqlCode in internal error codes for easier error handling
+     * @param sqlCode
+     * @return -999 for unknown error
+     * -100 for not found
+     * -200 for duplicates
+     * -300 for foreign key locks
+     * -400 for other locks --> try it later again
+     * -500 for wrong values
+     * -501 for nullable errors
+     * -502 for values to long
+     * -600 for wrong SQLQueries (should not occur in production)
+     */
     public int switchSQLError(int sqlCode) {
         switch (sqlCode){
-
-            default:    return -1;
+            /*
+            case notFound:      return -100;
+            case duplicate:     return -200;
+            case foreignKeys:   return -300;
+            case locks:         return -400;
+            case wrongVal:      return -500;
+            case nullable:      return -501;
+            case toLong:        return -502;
+            case SQLQuery:      return -600;
+            */
+            default:            return -999;
         }
         /*
-        switch (errorCode) {
-            case 1022: rows = -2;
-                case : rows = ;
-                case : rows = ;
-            default: rows = -1;
-        }
         1062 duplicate
         1022 duplicate
         1165 table locked
