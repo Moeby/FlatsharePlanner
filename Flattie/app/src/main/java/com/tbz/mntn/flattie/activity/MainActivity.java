8ca41b42659ab64de5a6e28436ad0187b327428a
moeby <tatyana.merlo@gmail.com>
2017-12-20 14:03:37 +0100

    splash added

--- Flattie/app/src/main/java/com/tbz/mntn/flattie/activity/MainActivity.java
+++ Flattie/app/src/main/java/com/tbz/mntn/flattie/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.tbz.mntn.flattie;
+package com.tbz.mntn.flattie.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tbz.mntn.flattie.R;
 
 public class MainActivity extends AppCompatActivity {
 
