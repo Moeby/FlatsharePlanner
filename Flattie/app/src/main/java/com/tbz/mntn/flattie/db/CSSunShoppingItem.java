638574514f4b411228a791bb95666eb9f0966b7f
teshi24 <nadja.stadi@gmail.com>
2017-12-22 16:53:54 +0100

    #60: first trial and notes CheckStyle, some structure change in planning

---
+++ Flattie/app/src/main/java/com/tbz/mntn/flattie/db/CSSunShoppingItem.java
@@ -0,0 +1,104 @@
+package com.tbz.mntn.flattie.db;
+
+import android.support.annotation.Nullable;
+
+/**
+ * Class for ShoppingItems.
+ */
+public class CSSunShoppingItem {
+
+    /**
+     * an id.
+     */
+    private int id;
+
+    /**
+     * a name.
+     */
+    private String name;
+
+    /**
+     * a bought.
+     */
+    private boolean bought;
+
+    /**
+     * a group.
+     */
+    private Group group;
+
+    /**
+     * Empty constructor which creates an empty ShoppingItem.
+     * - set required attributes with getter and setter methods
+     */
+    public CSSunShoppingItem() {
+    }
+
+    /**
+     * @param name   of ShoppingItem
+     * @param bought true: the item gets bought, false: item is in list
+     * @param group  which contains this ShoppingItem
+     */
+    public CSSunShoppingItem(final String name, final boolean bought,
+                             @Nullable final Group group) {
+        this.name = name;
+        this.bought = bought;
+        this.group = group;
+    }
+
+    /**
+     * @return id of ShoppingItem
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id of ShoppingItem
+     */
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name of ShoppingItem
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name of ShoppingItem
+     */
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return if ShoppingItem is bought
+     */
+    public boolean isBought() {
+        return bought;
+    }
+
+    /**
+     * @param bought of ShoppingItem
+     */
+    public void setBought(final boolean bought) {
+        this.bought = bought;
+    }
+
+    /**
+     * @return group of ShoppingItem
+     */
+    public Group getGroup() {
+        return group;
+    }
+
+    /**
+     * @param group of ShoppingItem
+     */
+    public void setGroup(final Group group) {
+        this.group = group;
+    }
+}
--- planning/CodeConventions/CheckStyle-IDEA.md
+++ planning/CodeConventions/CheckStyle-IDEA.md
@@ -19,3 +19,15 @@ Advantages | Disadventages
 warnings, no force to change them | warnings, everybody can ignore different of them
 _ | a lot of issues
 _ | indents check
+
+## Notes
+ShoppingItem und ShoppingItemDAO angepasst
+SUN:
+Data - 41 E 8.37 - 8.48 --> what is 'verbirgt ein Feld'??
+DAO - 51 E 16.16 - 16.31
+Google:
+Data - 40 W 16.32 - 16.36  // 36 because of indent
+DAO - 172 W 16.36 - 16.40 // 148 because of indent
+
+Fazit: Google indent anpassen auf 4 / 8, name DAO nicht erlaubt streichen, check einfügen final in method (zuerst vielleicht schauen ob prg noch läuft und was der vorteil sein soll), check for package-info einbauen, beschrieb return tag einbauen, javadoc für methoden und klassen required machen, jedoch getter und setter ignorieren
+
