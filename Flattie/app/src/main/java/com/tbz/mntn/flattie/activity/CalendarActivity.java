5280ff15aed698c3849d9e824f5a4fe6b6606c6e
unknown <nivram.reknehcs@gmail.com>
2018-01-04 13:07:20 +0100

    #58 CalendarActivity created

---
+++ Flattie/app/src/main/java/com/tbz/mntn/flattie/activity/CalendarActivity.java
@@ -0,0 +1,20 @@
+package com.tbz.mntn.flattie.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tbz.mntn.flattie.R;
+
+/**
+ * Created by Marvin on 04.01.2018.
+ */
+
+public class CalendarActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar);
+
+    }
+
+}
