ae7f15635752a2bd91caba707206e66716335f79
teshi24 <nadja.stadi@gmail.com>
2018-01-09 21:28:56 +0100

    merge mysqlconnectortest

--- Flattie/app/src/test/java/com/tbz/mntn/flattie/database/databaseConnection/MysqlConnectorTest.java
+++ Flattie/app/src/test/java/com/tbz/mntn/flattie/database/databaseConnection/MysqlConnectorTest.java
@@ -1,10 +1,7 @@
 package com.tbz.mntn.flattie.database.databaseConnection;
-<<<<<<< HEAD:Flattie/app/src/test/java/com/tbz/mntn/flattie/database/databaseConnection/MysqlConnectorTest.java
-=======
 
 import org.junit.Ignore;
 import org.junit.Test;
->>>>>>> 8594c53073daf5c08c66458c7e264a23b6bd76b1:Flattie/app/src/test/java/com/tbz/mntn/flattie/database/databaseConnection/MysqlConnectorTest.java
 
 import java.sql.Connection;
 import java.sql.SQLException;
