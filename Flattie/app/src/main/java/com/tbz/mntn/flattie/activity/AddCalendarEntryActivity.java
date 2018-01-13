0278201e260137d14ad2d1d6a8899d0f521ea55f
moeby <tatyana.merlo@gmail.com>
2018-01-13 21:32:22 +0100

    #61 Add calendar entry

--- Flattie/app/src/main/java/com/tbz/mntn/flattie/activity/AddCalendarEntryActivity.java
+++ Flattie/app/src/main/java/com/tbz/mntn/flattie/activity/AddCalendarEntryActivity.java
@@ -1,19 +1,199 @@
 package com.tbz.mntn.flattie.activity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.github.jjobes.slidedatetimepicker.SlideDateTimeListener;
+import com.github.jjobes.slidedatetimepicker.SlideDateTimePicker;
 import com.tbz.mntn.flattie.R;
+import com.tbz.mntn.flattie.authentication.LoggedInUser;
+import com.tbz.mntn.flattie.calendar.CalendarEntryController;
+import com.tbz.mntn.flattie.database.dao.DaoFactory;
+import com.tbz.mntn.flattie.database.dao.EventCategoryDao;
+import com.tbz.mntn.flattie.database.dataclasses.EventCategory;
+import com.tbz.mntn.flattie.database.dataclasses.Repeatable;
+import com.tbz.mntn.flattie.database.dataclasses.User;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 /**
- * Created by Marvin on 04.01.2018.
+ * Activity for adding an calendar event.
  */
-
 public class AddCalendarEntryActivity extends AppCompatActivity {
+  private Date startDate;
+  private Date endDate;
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_calentar_entry);
+  private SlideDateTimeListener listener = new SlideDateTimeListener() {
+    @SuppressLint("SimpleDateFormat")
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd yyyy hh:mm aa");
 
+    /**
+     * DateTime set on Datetimepicker.
+     * @param date  The {@code Date} object that contains the date
+     */
+    @Override
+    public void onDateTimeSet(Date date) {
+      startDate = date;
+      TextInputLayout startDate = findViewById(R.id.cal_start_date);
+      if (endDate == null || date.before(endDate)) {
+        startDate.getEditText().setText(dateFormat.format(date));
+      } else {
+        startDate.getEditText().setText("");
+        // show error hint
+        Toast.makeText(AddCalendarEntryActivity.this,
+                       "Startdate cannot be later than enddate. Try again",
+                       Toast.LENGTH_LONG).show();
+      }
     }
+
+    // Optional cancel listener
+    @Override
+    public void onDateTimeCancel() {
+      Toast.makeText(AddCalendarEntryActivity.this,
+                     "Canceled", Toast.LENGTH_SHORT).show();
+    }
+  };
+
+  private SlideDateTimeListener listener2 = new SlideDateTimeListener() {
+    @SuppressLint("SimpleDateFormat")
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd yyyy hh:mm aa");
+
+    /**
+     * DateTime set on Datetimepicker.
+     * @param date  The {@code Date} object that contains the date
+     */
+    @Override
+    public void onDateTimeSet(Date date) {
+      endDate = date;
+      TextInputLayout endDate = findViewById(R.id.cal_end_date);
+      if (startDate == null || date.after(startDate)) {
+        endDate.getEditText().setText(dateFormat.format(date));
+      } else {
+        endDate.getEditText().setText("");
+        // show error hint
+        Toast.makeText(AddCalendarEntryActivity.this,
+                       "Enddate cannot be earlier than startdate. Try again",
+                       Toast.LENGTH_LONG).show();
+      }
+    }
+
+    // Optional cancel listener
+    @Override
+    public void onDateTimeCancel() {
+      Toast.makeText(AddCalendarEntryActivity.this,
+                     "Canceled", Toast.LENGTH_SHORT).show();
+    }
+  };
+
+  @RequiresApi(api = Build.VERSION_CODES.N)
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.add_calentar_entry);
+    User loggedInUser = LoggedInUser.getLoggedInUser();
+
+    final EditText        startDate         = findViewById(R.id.cal_start_date_input);
+    final EditText        endDate           = findViewById(R.id.cal_end_date_input);
+    final Button          submit            = findViewById(R.id.btn_add_cal_entry);
+    final TextInputLayout description       = findViewById(R.id.cal_entry_layout_desc);
+    final Spinner         repeatableSpinner = findViewById(R.id.cal_entry_repeatable);
+    final Spinner         categorySpinner   = findViewById(R.id.cal_entry_category);
+    final Spinner         peopleSpinner     = findViewById(R.id.cal_entry_people);
+
+    setRepeatableSpinnerValues(repeatableSpinner);
+    setCategorySpinnerValues(categorySpinner);
+    setGroupMembersSpinnerValues(peopleSpinner);
+
+    addListenersToDateFields(startDate, endDate);
+
+    submit.setOnClickListener(v -> {
+      // Check dates and description are not empty
+      if (this.startDate != null && this.endDate != null) {
+        CalendarEntryController calendarEntryController = new CalendarEntryController();
+        Boolean isItemSavedToDb =
+            calendarEntryController.saveCalendarEntryToDatabase(description.getEditText().getText().toString(),
+                                                                repeatableSpinner.getSelectedItem().toString(),
+                                                                this.startDate,
+                                                                this.endDate,
+                                                                loggedInUser.getGroup(),
+                                                                categorySpinner.getSelectedItem().toString(),
+                                                                peopleSpinner.getSelectedItem().toString());
+        if (isItemSavedToDb) {
+          launchCalendarActivity();
+        } else {
+          Snackbar.make(v, "Item could not be saved. Please try again.", 1000).show();
+        }
+      } else {
+        Snackbar.make(v, "Date missing.", 1000).show();
+      }
+    });
+  }
+
+  private void addListenersToDateFields(EditText startDate, EditText endDate) {
+    startDate.setOnClickListener(v -> new SlideDateTimePicker.Builder(getSupportFragmentManager())
+        .setListener(listener)
+        .setInitialDate(new Date())
+        .setMinDate(new Date())
+        .build()
+        .show());
+
+    endDate.setOnClickListener(v -> new SlideDateTimePicker.Builder(getSupportFragmentManager())
+        .setListener(listener2)
+        .setInitialDate(new Date())
+        .setMinDate(new Date())
+        .build()
+        .show());
+  }
+
+  private void setRepeatableSpinnerValues(Spinner repeatableSpinner) {
+    String[] repeatableNames = Arrays.toString(Repeatable.values()).replaceAll("^.|.$", "").split(", ");
+
+    ArrayAdapter<String> repeatableSpinnerArrayAdapter = new ArrayAdapter<>(
+        this, android.R.layout.simple_spinner_item, repeatableNames);
+    repeatableSpinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    repeatableSpinner.setAdapter(repeatableSpinnerArrayAdapter);
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.N)
+  private void setCategorySpinnerValues(Spinner categorySpinner) {
+    EventCategoryDao    eventCategoryDao = DaoFactory.getEventCategoryDao();
+    List<EventCategory> eventCategories  = eventCategoryDao.selectAll();
+    String[]            categoryNames    = eventCategories.stream().map(EventCategory::getName).toArray(String[]::new);
+
+    ArrayAdapter<String> repeatableSpinnerArrayAdapter = new ArrayAdapter<>(
+        this, android.R.layout.simple_spinner_item, categoryNames);
+    repeatableSpinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    categorySpinner.setAdapter(repeatableSpinnerArrayAdapter);
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.N)
+  private void setGroupMembersSpinnerValues(Spinner peopleSpinner) {
+    User            loggedInUser = LoggedInUser.getLoggedInUser();
+    ArrayList<User> groupUsers   = loggedInUser.getGroup().getUsers();
+    String[]        peopleNames  = groupUsers.stream().map(User::getUsername).toArray(String[]::new);
+
+    ArrayAdapter<String> repeatableSpinnerArrayAdapter = new ArrayAdapter<>(
+        this, android.R.layout.simple_spinner_item, peopleNames);
+    repeatableSpinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    peopleSpinner.setAdapter(repeatableSpinnerArrayAdapter);
+  }
+
+  private void launchCalendarActivity() {
+    Intent intent = new Intent(AddCalendarEntryActivity.this, CalendarActivity.class);
+    startActivity(intent);
+  }
 }
---
+++ Flattie/app/src/main/java/com/tbz/mntn/flattie/calendar/CalendarEntryController.java
@@ -0,0 +1,83 @@
+package com.tbz.mntn.flattie.calendar;
+
+import com.tbz.mntn.flattie.database.dao.CalendarItemDao;
+import com.tbz.mntn.flattie.database.dao.DaoFactory;
+import com.tbz.mntn.flattie.database.dao.EventCategoryDao;
+import com.tbz.mntn.flattie.database.dao.UserDao;
+import com.tbz.mntn.flattie.database.dataclasses.CalendarItem;
+import com.tbz.mntn.flattie.database.dataclasses.EventCategory;
+import com.tbz.mntn.flattie.database.dataclasses.Group;
+import com.tbz.mntn.flattie.database.dataclasses.Repeatable;
+import com.tbz.mntn.flattie.database.dataclasses.User;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Calendar entry handling.
+ */
+public class CalendarEntryController {
+
+  /**
+   * Get the data objects for the specified parameters and try to persist it.
+   * @param description   of the event
+   * @param repeatable    (NONE, DAILY, WEEKLY, MONTHLY, YEARLY)
+   * @param startDate     of the event
+   * @param endDate       of the event
+   * @param group         the event belongs to
+   * @param eventCategory (event, absence, duty)
+   * @param assignedUser  to the event, only one possible
+   * @return true if persisting was successful, false otherwise
+   */
+  public boolean saveCalendarEntryToDatabase(String description,
+                                             String repeatable,
+                                             Date startDate,
+                                             Date endDate,
+                                             Group group,
+                                             String eventCategory,
+                                             String assignedUser) {
+    {
+      CalendarItemDao calendarItemDao = DaoFactory.getCalendarItemDao();
+      Repeatable      repeat          = Repeatable.toRepeatable(repeatable);
+
+      EventCategoryDao    eventCategoryDao = DaoFactory.getEventCategoryDao();
+      List<EventCategory> categories       = eventCategoryDao.selectAll();
+      EventCategory       category         = null;
+
+      UserDao   userDao        = DaoFactory.getUserDao();
+      User      assigned       = userDao.selectByUsername(assignedUser);
+      Timestamp startTimestamp = convertUtilDateToSqlTimestamp(startDate);
+      Timestamp endTimestamp   = convertUtilDateToSqlTimestamp(endDate);
+
+      for (EventCategory cat : categories) {
+        if (eventCategory.equals(cat.getName())) {
+          category = cat;
+          break;
+        }
+      }
+
+      CalendarItem item = new CalendarItem(description,
+                                           repeat,
+                                           startTimestamp,
+                                           endTimestamp,
+                                           group,
+                                           category,
+                                           assigned,
+                                           null);
+      int result = calendarItemDao.insert(item);
+
+      // insert successful == 1
+      if (result != 1) {return false;}
+      return true;
+    }
+  }
+
+  private Timestamp convertUtilDateToSqlTimestamp(Date dateToConvert) {
+    Calendar cal = Calendar.getInstance();
+    cal.setTime(dateToConvert);
+    cal.set(Calendar.MILLISECOND, 0);
+    return new java.sql.Timestamp(dateToConvert.getTime());
+  }
+}
