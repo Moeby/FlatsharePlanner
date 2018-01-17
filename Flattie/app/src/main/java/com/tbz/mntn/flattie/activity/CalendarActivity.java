4956d17583b773884012346aec661f8cbe3cc65f
teshi24 <nadja.stadi@gmail.com>
2018-01-17 14:52:51 +0100

    #58 review and changes
    DatePicker shows date from TextField

--- Flattie/app/src/main/java/com/tbz/mntn/flattie/activity/CalendarActivity.java
+++ Flattie/app/src/main/java/com/tbz/mntn/flattie/activity/CalendarActivity.java
@@ -1,44 +1,39 @@
 package com.tbz.mntn.flattie.activity;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.CalendarView;
 import android.widget.CalendarView.OnDateChangeListener;
 
 import com.tbz.mntn.flattie.R;
-import com.tbz.mntn.flattie.authentification.LoggedInUser;
+
+import java.util.Date;
 
 public class CalendarActivity extends AppCompatActivity {
 
   // intent variables
-  public static final String EXTRA_DAY = "day";
-  public static final String EXTRA_MONTH = "month";
-  public static final String EXTRA_YEAR = "year";
+  public static final String EXTRA_INT_DAY   = "day";
+  public static final String EXTRA_INT_MONTH = "month";
+  public static final String EXTRA_INT_YEAR  = "year";
 
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.calendar);
     CalendarView calendarView = findViewById(R.id.calendarView);
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-      calendarView.setOnDateChangeListener(new OnDateChangeListener() {
-        @Override
-        public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
-          //Toast.makeText(getApplicationContext(), "" + day, Toast.LENGTH_SHORT).show();
-          launchAddCalendarEntryActivity(year, month, day);
-        }
-      });
-    }
+    calendarView.setOnDateChangeListener(new OnDateChangeListener() {
+      @Override
+      public void onSelectedDayChange(CalendarView view, int year, int month, int day) {
+        //Toast.makeText(getApplicationContext(), "" + day, Toast.LENGTH_SHORT).show();
+        launchAddCalendarEntryActivity(year, month, day);
+      }
+    });
   }
 
   /**
    * Create Menu, logout function
-   *
-   * @param
-   * @return
+   * @param menu todo add text
+   * @return todo add text
    */
   /*@Override
   public boolean onCreateOptionsMenu(Menu menu) {
@@ -55,12 +50,11 @@ public class CalendarActivity extends AppCompatActivity {
     });
     return super.onCreateOptionsMenu(menu);
   }*/
-
   private void launchAddCalendarEntryActivity(int year, int month, int day) {
     Intent intent = new Intent(getBaseContext(), AddCalendarEntryActivity.class);
-    intent.putExtra(EXTRA_DAY, day);
-    intent.putExtra(EXTRA_MONTH, month);
-    intent.putExtra(EXTRA_YEAR, year);
+    intent.putExtra(EXTRA_INT_DAY, day);
+    intent.putExtra(EXTRA_INT_MONTH, month);
+    intent.putExtra(EXTRA_INT_YEAR, year);
     startActivity(intent);
   }
 }
